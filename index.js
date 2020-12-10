9b8d2049f86f5f48ed78c2c143284cdd7b52db76
socratone <gim2origin@gmail.com>
2020-12-10 14:48:58 +0900

    기본 서버 설정

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const routes = require('./routes');
+
+app.use(cors());
+app.use(express.json()); 
+ 
+app.use('/api/items', routes.items);
+
+app.listen(3001, () => console.log('서버가 3001 포트에서 가동 중입니다.'));
