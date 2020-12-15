97bf8e238a6d85641c17b361cea028b6f23a2b1f
socratone <gim2origin@gmail.com>
2020-12-15 18:28:28 +0900

    item 업로드 기능 구현

--- src/routes/item.js
+++ src/routes/item.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { query } = require('../database/model');
 
-router.post('/', (req, res) => {
-  
-  res.status(200).send('post id');
+router.post('/', async (req, res) => {
+  let { title, description, price, category, storeLink, imageFileName } = req.body;
+  if (!description) description = null;
+  if (!price) price = null;
+  if (!storeLink) price = null;
+
+  try {
+    // category에 해당하는 id를 알아낸다.
+    const sql = 'SELECT id FROM category WHERE name = ?';
+    const results = await query(sql, [category]);
+    const categoryId = results[0].id;
+
+    // item_order 중 가장 큰 숫자를 찾아 1을 더한다.
+    const sql2 = 'SELECT MAX(item_order) AS maxOrder FROM item';
+    const results2 = await query(sql2);
+    const itemOrder = results2[0].maxOrder + 1;
+
+    // insert
+    const sql3 = `
+      INSERT INTO item (title, description, price, category_id, main_image_URL, 
+        smart_store, item_order, created_at) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`;
+    const results3 = await query(sql3, [title, description, price, categoryId, imageFileName, storeLink, itemOrder]);
+
+    res.status(200).send(results3);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = router;
