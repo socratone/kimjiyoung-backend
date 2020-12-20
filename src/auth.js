96ea52f807b4c738c8adbae9229b3a28b4d2a8cf
socratone <gim2origin@gmail.com>
2020-12-20 10:50:45 +0900

    토큰 에러 핸들링

--- src/auth.js
+++ src/auth.js
@@ -9,7 +9,11 @@ const auth = (req, res, next) => {
     req.user = decoded; // 디코딩이 잘 되면 user 객체에 담아 다음으로 넘겨준다.
     next();
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send({ 
+      error: { 
+        message: `토큰이 만료 됐거나 잘못됐습니다. 다시 로그인 해주세요.`
+      }
+    });
   }
 }
 
