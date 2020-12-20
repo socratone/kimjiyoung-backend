9471302d8d7f918a0b3e08a980159a6ef0112989
socratone <gim2origin@gmail.com>
2020-12-20 11:31:16 +0900

    signup 리팩토링

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -10,21 +10,17 @@ router.post('/', async (req, res) => {
     const sql = 'SELECT email FROM user WHERE email = ?';
     const results = await query(sql, [email]);
     if (results.length > 0) {
-      return res.status(403).send({ message: '이미 사용하고 있는 이메일입니다.'});
+      return res.status(403).send({ error: { message: '이미 사용하고 있는 이메일입니다.' }});
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashed = await bcrypt.hash(password, salt);
   
     const sql2 = 'INSERT INTO user (email, password, name) VALUES (?, ?, ?)';
-    const results2 = await query(sql2, [email, hashed, name]);
-    if (results2.affectedRows === 1) {
-      res.status(200).send({ message: `${email}이라는 아이디를 생성했습니다.`});
-    } else {
-      res.status(500).send({ message: '아이디를 생성하지 못했습니다.'});
-    }
+    await query(sql2, [email, hashed, name]);
+    res.status(200).send({ email: email });
   } catch (error) {
-    res.status(500).send({ message: error.message });
+    res.status(500).send({ error: { message: error.message }});
   }
 });
 
