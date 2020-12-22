6491413bfb309a4733a77fbfb8539211521d15f1
socratone <gim2origin@gmail.com>
2020-12-22 15:04:48 +0900

    word를 text로 수정해서 데이터 전송

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -5,7 +5,7 @@ const { query } = require('../database/model');
 
 router.get('/', async (req, res) => {
   try {
-    const sql = 'SELECT * FROM profile ORDER BY sort, id';
+    const sql = 'SELECT id, sort, image, word AS text FROM profile ORDER BY sort, id';
     const results = await query(sql);
 
     const profile = { 
@@ -17,11 +17,11 @@ router.get('/', async (req, res) => {
 
     for (let i = 0; i < results.length; i++) {
       if (results[i].image === 'first-item.png') {
-        profile.firstItem['text'] = results[i].word;
+        profile.firstItem['text'] = results[i].text;
       } else if (results[i].image === 'second-item.png') {
-        profile.secondItem['text'] = results[i].word;
+        profile.secondItem['text'] = results[i].text;
       } else if (results[i].image === 'third-item.png') {
-        profile.thirdItem['text'] = results[i].word;
+        profile.thirdItem['text'] = results[i].text;
       } else {
         profile.items.push(results[i]);
       }
@@ -38,6 +38,17 @@ router.put('/', auth, async (req, res) => {
     let { image, text } = req.body;
     const sql = 'UPDATE profile SET word = ? WHERE image = ?';
     const results = await query(sql, [text, image]);
+    res.status(200).send(results);
+  } catch (error) {
+    res.status(500).send({ error: { message: error.message }});
+  }
+});
+
+router.post('/item', auth, async (req, res) => {
+  try {
+    let { description, imageFileName } = req.body;
+    const sql = `INSERT INTO profile (image, word, sort) VALUES (?, ?, 'sub')`;
+    const results = await query(sql, [imageFileName, description]);
     res.status(200).send(results);
   } catch (error) {
     res.status(500).send({ error: { message: error.message }});
